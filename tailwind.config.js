02497cca191a6c739febd6f0972e5033c070ae58
evavandongen <eva.van.dongen@usmedia.nl>
2024-02-19 14:54:30 +0100

    recipe page

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,12 +10,17 @@ module.exports = {
         },
         colors: {
             'white': 'hsl(0,0%,100%)',
-            'gray': 'hsl(212, 45%, 89%)',
-            'blue': 'hsl(220, 15%, 55%)',
-            'blue-dark': 'hsl(218, 44%, 22%)',
+            'gray-light': 'hsl(30, 18%, 87%)',
+            'charcoal': 'hsl(24, 5%, 18%)',
+            'brown': 'hsl(30, 10%, 34%)',
+            'nutmeg': 'hsl(14, 45%, 36%)',
+            'dark-raspberry': 'hsl(332, 51%, 32%)',
+            'rose-white': 'hsl(330, 100%, 98%)',
+            'eggshell': 'hsl(30, 54%, 90%)',
         },
         fontFamily: {
             sans: ['Outfit', 'sans-serif'],
+            serif: ['Young Serif', 'serif'],
         },
         screens: {
             'sm': '480px',
@@ -23,12 +28,7 @@ module.exports = {
             'lg': '1024px',
             'xl': '1280px',
         },
-        extend: {
-            spacing: {
-            'p-gutter': '20px',
-            'p-gutter--large': '40px',
-            }
-        }
+        extend: {}
     },
     plugins: [],
 }
